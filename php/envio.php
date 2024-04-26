f42f2f10636b82cecfc1622afb77ed8db658b091
romi <romicatencio@gmail.com>
2024-04-26 14:46:47 -0300

    cambios en el formulario

--- php/envio.php
+++ php/envio.php
@@ -1,25 +1,19 @@
+
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-  
-// Obtener datos del formulario
+    // Obtener datos del formulario
     $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
     
- 
-$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    
-   
-$message = trim($_POST["message"]);
-    
-    
- 
-// Asegúrate de cambiar esta dirección de correo electrónico al destinatario deseado
+    // Asegúrate de cambiar esta dirección de correo electrónico al destinatario deseado
     $recipient = "romicatencio@gmail.com";
     
-    
-    
- 
-// Asunto del correo
+    // Asunto del correo
     $subject = "Nueva consulta de $name";
     
     // Construye el contenido del correo
@@ -27,28 +21,17 @@ $message = trim($_POST["message"]);
     $email_content .= "Correo Electrónico: $email\n\n";
     $email_content .= "Consulta:\n$message\n";
     
-    
-    
-    /
-// Construye las cabeceras del correo
+    // Construye las cabeceras del correo
     $email_headers = "From: $name <$email>";
     
     // Envía el correo
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        
- 
-// Redireccionar al usuario a una página de agradecimiento después de enviar el formulario
-        
-        he
-header("Location: gracias.html");
+        // Redireccionar al usuario a una página de agradecimiento después de enviar el formulario
+        header("Location: gracias.html");
         exit;
-    } 
-    }
-else {
+    } else {
         // Si falla el envío del correo, mostrar un mensaje de error
-        
-   
-echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
+        echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
     }
 } else {
     // Si no es una solicitud POST, redirigir al usuario a la página de inicio
