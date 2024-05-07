759ab67b88976b5a2efd797db7f27e3670c8c9be
romi <romicatencio@gmail.com>
2024-05-06 22:44:01 -0300

    seguimos con cambios

--- js/contacto.js
+++ js/contacto.js
@@ -1,10 +1,10 @@
 function enviarFormulario() {
     var formData = new FormData(document.getElementById('contact-form'));
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'contacto.php', true);
+    xhr.open('POST', '../contacto.php', true);
     xhr.onload = function() {
         if (xhr.status === 200) {
-            console.log('Formulario enviado correctamente');
+            console.log('../gracias.html');
             // Aquí puedes realizar acciones adicionales después de enviar el formulario, como mostrar un mensaje de éxito o redireccionar a otra página
         } else {
             console.error('Error al enviar el formulario');
